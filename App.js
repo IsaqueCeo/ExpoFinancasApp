cdaa3389fe9e6f48a109c90fb4d74798d6d95e76
IsaqueCeo <isaqueabraaodesousa0000@gmail.com>
2024-10-07 19:38:57 -0300

    Commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect} from 'react';
+import * as SplashScreen from 'expo-splash-screen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+// import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+SplashScreen.preventAutoHideAsync();
+
+export default function App(){
+  useEffect(() =>{
+    setTimeout(async()=> {
+      await SplashScreen.hideAsync();
+    },3000)
+  },[])
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={LoginScreen}/>
+        {/* <Stack.Screen name='Home' component={HomeScreen}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
